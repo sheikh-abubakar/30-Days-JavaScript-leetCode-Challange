cd5ed4ee747673425838148176fee6171373eab7
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-07-22 13:54:16 +0500

    Add map function to transform elements in an array of problem5

---
+++ Problem5.js
@@ -0,0 +1,14 @@
+// Apply Transform Over Each Element in Array
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const newArr = [];
+    for(let i =0; i< arr.length; i++){
+        newArr[i] = fn(arr[i], i)
+    }
+    return newArr
+};
