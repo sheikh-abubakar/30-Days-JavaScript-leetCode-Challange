9f5cba92fc901ab4941ddf5035a9e2faba444882
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-07-29 15:44:28 +0500

    Problems 9, 10 and 11 solved

---
+++ Problem11.js
@@ -0,0 +1,32 @@
+// Memoize
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache = {}
+    return function(...args) {
+        let n = JSON.stringify(args)
+        if(n in cache){
+            return cache[n]
+        }else{
+            let res = fn.apply(this, args)
+            cache[n] = res
+            return res
+        }
+
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
