4f3dd45b85a9c35504977c0f2be91c3a6bf77f79
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-08-07 13:41:20 +0500

    problem 16 of Promise Time Limit solved

---
+++ Problem16.js
@@ -0,0 +1,25 @@
+//Promise Time Limit
+
+// setTimeOut fn is a async function and we use promise to handle it
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        return new Promise((res, rej)=>
+        {
+            setTimeout(()=>{rej("Time Limit Exceeded")},t)
+
+            fn(...args).then(res).catch(rej)
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
