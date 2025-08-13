2fb654642c9069aa7fd4dea0b8bdec7f0734ec7c
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-08-13 13:53:05 +0500

    problem 21 solved

---
+++ Problem21.js
@@ -0,0 +1,14 @@
+// chunk array
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let array =  []
+    for(let i = 0; i<arr.length; i+= size){
+        array.push(arr.slice(i, i+size))
+    }
+    return array;
+};
