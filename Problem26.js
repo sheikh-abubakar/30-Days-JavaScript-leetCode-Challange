cfb4c78ae2526b557e0267413da186af17254e85
sheikh-abubakar <abubakarjavaid034@gmail.com>
2025-08-17 13:13:10 +0500

    problem 26 solved

---
+++ Problem26.js
@@ -0,0 +1,23 @@
+//  Flatten Deeply Nested Array
+
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if(n === 0){
+        return arr.slice()
+    }
+
+    let flatArr = []
+    for(let i = 0; i<arr.length; i++){
+        if(Array.isArray(arr[i])){
+            const nested = flat(arr[i], n-1)
+            flatArr.push(...nested)
+        }else{
+            flatArr.push(arr[i])
+        }
+    }
+    return flatArr;
+};
